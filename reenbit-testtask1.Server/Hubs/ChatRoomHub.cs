bb34f94cda02aa04f19143429a24020a793a81cb
Nalzurin <alexnormandi@ukr.net>
2025-05-09 18:22:34 +0300

    Implement ChatRoom Hub

---
+++ reenbit-testtask1.Server/Hubs/ChatRoomHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace reenbit_testtask1.Server.Hubs
+{
+    public class ChatRoomHub : Hub
+    {
+        public Task BroadcastMessage(string name, string message) =>
+            Clients.All.SendAsync("broadcastMessage", name, message);
+
+        public Task Echo(string name, string message) =>
+            Clients.Client(Context.ConnectionId)
+                    .SendAsync("echo", name, $"{message} (echo from server)");
+    }
+}
--- reenbit-testtask1.Server/Program.cs
+++ reenbit-testtask1.Server/Program.cs
@@ -1,4 +1,6 @@
 
+using reenbit_testtask1.Server.Hubs;
+
 namespace reenbit_testtask1.Server
 {
     public class Program
@@ -6,7 +8,7 @@ namespace reenbit_testtask1.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddSignalR().AddAzureSignalR();
             // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -30,7 +32,7 @@ namespace reenbit_testtask1.Server
 
 
             app.MapControllers();
-
+            app.MapHub<ChatRoomHub>("/chatroom");
             app.MapFallbackToFile("/index.html");
 
             app.Run();
